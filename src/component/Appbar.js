9c7360699aad335d38596ceb9e65c946150ec92f
chanmarsh@06chandrasekaranmca06@gmail.com <you@example.com>
2023-07-19 16:23:42 +0530

    teasting github

---
+++ src/component/Appbar.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import img1 from '../img/henna-painted-hand.png'
+import Cart from './Cart';
+
+function Appbar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <h2 className="text-primary">
+            <img
+              src={img1}
+              className="d-inline-block  align-bottom mb-2"
+              style={{ width: "20px" }}
+              alt=""
+            />{" "}
+            Henna Artist
+          </h2>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="#home" className="fw-bold">
+              Home
+            </Nav.Link>
+            {/* <Nav.Link href="#link">Services</Nav.Link> */}
+            <Nav.Link href="#menu"  className="fw-bold">
+              Menu
+            </Nav.Link>
+            <Nav.Link href="#musttry" className="fw-bold">
+              Must Try
+            </Nav.Link>
+            <Nav.Link href="#contact" className="fw-bold">
+              Contact Us
+            </Nav.Link>
+            <Cart/>
+
+            {/* <NavDropdown
+              title="Contact Us"
+              className="fw-bold"
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Appbar
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+// custom theme
+import "./css/main.min.css";
+// icon 
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import './index.css'
+import App from "./App.js";
+
+ReactDOM.render(<App/>, document.getElementById("app"));
